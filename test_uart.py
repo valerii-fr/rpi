1008a3d91cf8c6c9b16f80bc66cf6adcebb4a87a
palevominimale <92236559+palevominimale@users.noreply.github.com>
2021-10-26 02:27:05 +0300

    Add files via upload

---
+++ test_uart.py
@@ -0,0 +1,29 @@
+import serial
+import time
+
+ser = serial.Serial('/dev/ttyS0', 19200, timeout=1)
+ser.flush()
+
+json_data = {
+	"act":1,
+	"spd":128,
+	"angle_v":90,
+	"angle_h":90,
+	"laser_i":128
+	}
+    
+json_data2 = {
+	"act":0,
+	"spd":128,
+	"angle_v":90,
+	"angle_h":90,
+	"laser_i":128
+	}
+    
+while True:
+    str = ser.write(json_data)
+    print(str)
+    time.sleep(250)
+    str = ser.write(json_data2)
+    print(str)
+    time.sleep(250)
