2ba2dfcc194495dce7adc6efd4c26f8cd6f87865
palevominimale <92236559+palevominimale@users.noreply.github.com>
2021-10-26 22:35:17 +0300

    Add files via upload

--- bt_controller.py
+++ bt_controller.py
@@ -40,12 +40,12 @@ for event in vrbox.read_loop():
             print("X: {x}" .format(x=event.value))
             if event.value > 0:
                 act = 3
-                spd = 128 + event.value * 42
+                spd = 128 + event.value * 3.5
                 data_set = {"act":act, "spd":spd, "angle_v":angle_v, "angle_h":angle_h, "laser_i":laser_i}
                 json_str = json.dumps(data_set)
                 print(json_str)
                 ser.write(str(json_str) .encode('ascii'))
-                time.sleep(0.03)
+                time.sleep(0.02)
                 act = 0
                 data_set = {"act":act, "spd":spd, "angle_v":angle_v, "angle_h":angle_h, "laser_i":laser_i}
                 json_str = json.dumps(data_set)
@@ -53,12 +53,12 @@ for event in vrbox.read_loop():
                 ser.write(str(json_str) .encode('ascii'))
             elif event.value < 0:
                 act = 4
-                spd = 128 + event.value * (-42)
+                spd = 128 + event.value * (-3.5)
                 data_set = {"act":act, "spd":spd, "angle_v":angle_v, "angle_h":angle_h, "laser_i":laser_i}
                 json_str = json.dumps(data_set)
                 print(json_str)
                 ser.write(str(json_str) .encode('ascii'))
-                time.sleep(0.03)
+                time.sleep(0.02)
                 act = 0
                 data_set = {"act":act, "spd":spd, "angle_v":angle_v, "angle_h":angle_h, "laser_i":laser_i}
                 json_str = json.dumps(data_set)
