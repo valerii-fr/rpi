f801cb9147b41a1ba645294a5732cc6be68760c9
palevominimale <92236559+palevominimale@users.noreply.github.com>
2021-10-26 23:18:50 +0300

    Add files via upload

--- bt_controller.py
+++ bt_controller.py
@@ -1,6 +1,7 @@
 from evdev import InputDevice, categorize, ecodes
 import serial
 import time
+import threading
 import json
 
 print("VR BOX Controller test")
@@ -20,12 +21,27 @@ spd = 128                       #PWM motor speed control
 angle_h = 90                    #laser servo horizontal angle (90 - center)
 angle_v = 90                    #laser servo vertical angle (90 - center)
 laser_i = 128                   #laser intensity
+flag = 0
 
 #json_stop = "{\"act\":0,\"spd\":150,\"angle_v\":90,\"angle_h\":90,\"laser_i\":128} \n"
 data_set = {"act":act, "spd":spd, "angle_v":angle_v, "angle_h":angle_h, "laser_i":laser_i}
 json_str = json.dumps(data_set)
 ser = serial.Serial('/dev/ttyS0', 19200, timeout=1)
 ser.flush()
+
+def check_flag():
+    if flag == 0:
+        nop()
+    elif flag == 1:
+        act = 0
+        spd = 0
+        data_set = {"act":act, "spd":spd, "angle_v":angle_v, "angle_h":angle_h, "laser_i":laser_i}
+        json_str = json.dumps(data_set)
+        print(json_str)
+        ser.write(str(json_str) .encode('ascii'))
+    threading.Timer(0.02, check_flag).start()
+
+check_flag()
 
 for event in vrbox.read_loop():
     if event.type == ecodes.EV_KEY:
@@ -45,6 +61,7 @@ for event in vrbox.read_loop():
         if event.code == x_var:
             print("X: {x}" .format(x=event.value))
             if event.value > 0:
+                flag = 0
                 act = 1
                 spd = 128 + event.value * 3.5
                 data_set = {"act":act, "spd":spd, "angle_v":angle_v, "angle_h":angle_h, "laser_i":laser_i}
@@ -57,16 +74,20 @@ for event in vrbox.read_loop():
                 #json_str = json.dumps(data_set)
                 #print(json_str)
                 #ser.write(str(json_str) .encode('ascii'))
+                flag = 1
             if event.value < 0:
+                flag = 0
                 act = 2
                 spd = 128 + event.value * (-3.5)
                 data_set = {"act":act, "spd":spd, "angle_v":angle_v, "angle_h":angle_h, "laser_i":laser_i}
                 json_str = json.dumps(data_set)
                 print(json_str)
                 ser.write(str(json_str) .encode('ascii'))
+                flag = 1
         elif event.code == y_var:
             print("Y: {y}".format(y=event.value))
             if event.value < 0:
+                flag = 0
                 act = 3
                 spd = 128 + event.value * (-3.5)
                 data_set = {"act":act, "spd":spd, "angle_v":angle_v, "angle_h":angle_h, "laser_i":laser_i}
@@ -79,7 +100,9 @@ for event in vrbox.read_loop():
                 #json_str = json.dumps(data_set)
                 #print(json_str)
                 #ser.write(str(json_str) .encode('ascii'))
+                flag = 1
             if event.value > 0:
+                flag = 0
                 act = 4
                 spd = 128 + event.value * 3.5
                 data_set = {"act":act, "spd":spd, "angle_v":angle_v, "angle_h":angle_h, "laser_i":laser_i}
@@ -92,5 +115,6 @@ for event in vrbox.read_loop():
                 #json_str = json.dumps(data_set)
                 #print(json_str)
                 #ser.write(str(json_str) .encode('ascii'))
+                flag = 1
             #if event.value == 0:
                 
