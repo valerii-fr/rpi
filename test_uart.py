68a14b6b10f3f4a06abd44bcab27ef09ad521181
palevominimale <92236559+palevominimale@users.noreply.github.com>
2021-10-26 03:01:44 +0300

    Add files via upload

--- test_uart.py
+++ test_uart.py
@@ -10,9 +10,9 @@ json_data = "{\"act\":1,\"spd\":128,\"angle_v\":90,\"angle_h\":90,\"laser_i\":12
 json_data2 = "{\"act\":0,\"spd\":128,\"angle_v\":90,\"angle_h\":90,\"laser_i\":128}"
     
 while True:
-    str1 = ser.write(b str(json_data))
+    str1 = ser.write(str(json_data) .encode('ascii'))
     print(str1)
     time.sleep(250)
-    str1 = ser.write(b str(json_data2))
+    str1 = ser.write(str(json_data2) .encode('ascii'))
     print(str1)
     time.sleep(250)
