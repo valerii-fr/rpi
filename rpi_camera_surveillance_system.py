a685713e1441c6a4627db23dc665165a6d0ec3ae
palevominimale <92236559+palevominimale@users.noreply.github.com>
2021-10-26 16:35:20 +0300

    Add files via upload

--- rpi_camera_surveillance_system.py
+++ rpi_camera_surveillance_system.py
@@ -8,6 +8,7 @@ import logging
 import socketserver
 from threading import Condition
 from http import server
+from multiprocessing import Process
 import serial
 import time
 import json
@@ -30,80 +31,93 @@ json_data = "{\"act\":1,\"spd\":150,\"angle_v\":90,\"angle_h\":90,\"laser_i\":12
     
 json_data2 = "{\"act\":0,\"spd\":150,\"angle_v\":80,\"angle_h\":80,\"laser_i\":128} \n"
 
-class StreamingOutput(object):
-    def __init__(self):
-        self.frame = None
-        self.buffer = io.BytesIO()
-        self.condition = Condition()
 
-    def write(self, buf):
-        if buf.startswith(b'\xff\xd8'):
-            # New frame, copy the existing buffer's content and notify all
-            # clients it's available
-            self.buffer.truncate()
-            with self.condition:
-                self.frame = self.buffer.getvalue()
-                self.condition.notify_all()
-            self.buffer.seek(0)
-        return self.buffer.write(buf)
+def capture_video():
+    class StreamingOutput(object):
+        def __init__(self):
+            self.frame = None
+            self.buffer = io.BytesIO()
+            self.condition = Condition()
 
-class StreamingHandler(server.BaseHTTPRequestHandler):
-    def do_GET(self):
-        if self.path == '/':
-            self.send_response(301)
-            self.send_header('Location', '/index.html')
-            self.end_headers()
-        elif self.path == '/index.html':
-            content = PAGE.encode('utf-8')
-            self.send_response(200)
-            self.send_header('Content-Type', 'text/html')
-            self.send_header('Content-Length', len(content))
-            self.end_headers()
-            self.wfile.write(content)
-        elif self.path == '/stream.mjpg':
-            self.send_response(200)
-            self.send_header('Age', 0)
-            self.send_header('Cache-Control', 'no-cache, private')
-            self.send_header('Pragma', 'no-cache')
-            self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
-            self.end_headers()
-            try:
-                while True:
-                    with output.condition:
-                        output.condition.wait()
-                        frame = output.frame
-                    self.wfile.write(b'--FRAME\r\n')
-                    self.send_header('Content-Type', 'image/jpeg')
-                    self.send_header('Content-Length', len(frame))
-                    self.end_headers()
-                    self.wfile.write(frame)
-                    self.wfile.write(b'\r\n')
-                    str1 = ser.write(str(json_data) .encode('ascii'))
-                    print(str1)
-                    time.sleep(0.25)
-                    str1 = ser.write(str(json_data2) .encode('ascii'))
-                    print(str1)
-                    time.sleep(0.25)
-            except Exception as e:
-                logging.warning(
-                    'Removed streaming client %s: %s',
-                    self.client_address, str(e))
-        else:
-            self.send_error(404)
-            self.end_headers()
+        def write(self, buf):
+            if buf.startswith(b'\xff\xd8'):
+                # New frame, copy the existing buffer's content and notify all
+                # clients it's available
+                self.buffer.truncate()
+                with self.condition:
+                    self.frame = self.buffer.getvalue()
+                    self.condition.notify_all()
+                self.buffer.seek(0)
+            return self.buffer.write(buf)
 
-class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
-    allow_reuse_address = True
-    daemon_threads = True
+    class StreamingHandler(server.BaseHTTPRequestHandler):
+        def do_GET(self):
+            if self.path == '/':
+                self.send_response(301)
+                self.send_header('Location', '/index.html')
+                self.end_headers()
+            elif self.path == '/index.html':
+                content = PAGE.encode('utf-8')
+                self.send_response(200)
+                self.send_header('Content-Type', 'text/html')
+                self.send_header('Content-Length', len(content))
+                self.end_headers()
+                self.wfile.write(content)
+            elif self.path == '/stream.mjpg':
+                self.send_response(200)
+                self.send_header('Age', 0)
+                self.send_header('Cache-Control', 'no-cache, private')
+                self.send_header('Pragma', 'no-cache')
+                self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
+                self.end_headers()
+                try:
+                    while True:
+                        with output.condition:
+                            output.condition.wait()
+                            frame = output.frame
+                        self.wfile.write(b'--FRAME\r\n')
+                        self.send_header('Content-Type', 'image/jpeg')
+                        self.send_header('Content-Length', len(frame))
+                        self.end_headers()
+                        self.wfile.write(frame)
+                        self.wfile.write(b'\r\n')
+                except Exception as e:
+                    logging.warning(
+                        'Removed streaming client %s: %s',
+                        self.client_address, str(e))
+            else:
+                self.send_error(404)
+                self.end_headers()
 
-with picamera.PiCamera(resolution='640x480', framerate=24) as camera:
-    output = StreamingOutput()
-    #Uncomment the next line to change your Pi's Camera rotation (in degrees)
-    #camera.rotation = 90
-    camera.start_recording(output, format='mjpeg')
-    try:
-        address = ('', 8000)
-        server = StreamingServer(address, StreamingHandler)
-        server.serve_forever()
-    finally:
-        camera.stop_recording()
+    class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
+        allow_reuse_address = True
+        daemon_threads = True
+
+    with picamera.PiCamera(resolution='640x480', framerate=24) as camera:
+        output = StreamingOutput()
+        #Uncomment the next line to change your Pi's Camera rotation (in degrees)
+        #camera.rotation = 90
+        camera.start_recording(output, format='mjpeg')
+        try:
+            address = ('', 8000)
+            server = StreamingServer(address, StreamingHandler)
+            server.serve_forever()
+        finally:
+            camera.stop_recording()
+
+def move_robot()
+    while True:
+        str1 = ser.write(str(json_data) .encode('ascii'))
+        print(str1)
+        time.sleep(0.25)
+        str1 = ser.write(str(json_data2) .encode('ascii'))
+        print(str1)
+        time.sleep(0.25)
+        
+if __name__ == '__main__':
+    p_video = Process(target=capture_video)
+    p_move = Process(target=move_robot)
+    p_video.start()
+    p_move.start()
+    p_video.join()
+    p_move.join()
