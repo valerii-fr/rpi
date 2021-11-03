e37fe016c1b0f3c650744cd9181ac360a31d89bd
palevominimale <92236559+palevominimale@users.noreply.github.com>
2021-11-03 19:23:58 +0300

    Add files via upload

--- ocp.py
+++ ocp.py
@@ -3,6 +3,8 @@ import  pyshine as ps
 from cv2 import CascadeClassifier
 import sys
 import numpy
+from camera import VideoCamera
+import camera
 HTML="""
 <html>
 <head>
@@ -20,26 +22,9 @@ def main():
     StreamProps.set_Page(StreamProps,HTML)
     address = ('192.168.1.102',9000) # Enter your IP address 
     try:
-        face_cascade = cv2.CascadeClassifier('/home/pi/downloads/opencv-master/data/haarcascades/haarcascade_frontalface_default.xml')
         StreamProps.set_Mode(StreamProps,'cv2')
-        capture = cv2.VideoCapture(0)
-        capture.set(cv2.CAP_PROP_BUFFERSIZE,4)
-        capture.set(cv2.CAP_PROP_FRAME_WIDTH,320)
-        capture.set(cv2.CAP_PROP_FRAME_HEIGHT,240)
-        capture.set(cv2.CAP_PROP_FPS,30)
-        ret, frame = capture.read()
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(
-            gray,
-            scaleFactor=1.1,
-            minNeighbors=5,
-            minSize=(30, 30),
-            flags=cv2.CASCADE_SCALE_IMAGE)
-        # Draw a rectangle around the faces
-        for (x, y, w, h) in faces:
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-        ret, frame = cv2.imencode('.jpg', capture)
-        StreamProps.set_Capture(StreamProps,capture)
+        strm = camera.get_frame()
+        StreamProps.set_Capture(StreamProps, strm)
         StreamProps.set_Quality(StreamProps,90)
         server = ps.Streamer(address,StreamProps)
         print('Server started at','http://'+address[0]+':'+str(address[1]))
