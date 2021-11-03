ee1bbf9264f36f7018b1ac2a2ab299df8927e60c
palevominimale <92236559+palevominimale@users.noreply.github.com>
2021-11-04 01:06:51 +0300

    Add files via upload

--- camera.py
+++ camera.py
@@ -19,20 +19,20 @@ json_str = json.dumps(data_set)
 ser = serial.Serial('/dev/ttyS0', 19200, timeout=1)
 ser.flush()
 
-class VideoCamera(object):
-    def stop_by_timer():
-        act = 0
-        spd = 0
-        data_set = {"act":act, "spd":spd, "angle_v":angle_v, "angle_h":angle_h, "laser_i":laser_i}
-        json_str = json.dumps(data_set)
-        print(json_str)
-        ser.write(str(json_str) .encode('ascii'))
-    
-    def newTimer():
-        global timeout_obj
-        timeout_obj = Timer(0.1, stop_by_timer)
-    newTimer() 
-        
+def stop_by_timer():
+    act = 0
+    spd = 0
+    data_set = {"act":act, "spd":spd, "angle_v":angle_v, "angle_h":angle_h, "laser_i":laser_i}
+    json_str = json.dumps(data_set)
+    print(json_str)
+    ser.write(str(json_str) .encode('ascii'))
+
+def newTimer():
+    global timeout_obj
+    timeout_obj = Timer(0.1, stop_by_timer)
+newTimer()
+
+class VideoCamera(object):      
     def __init__(self):
         # Using OpenCV to capture from device 0. If you have trouble capturing
         # from a webcam, comment the line below out and use a video file
