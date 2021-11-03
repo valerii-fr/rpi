c9715a25f8ebf6f5c14f952406ddbe0ddc2bb86f
palevominimale <92236559+palevominimale@users.noreply.github.com>
2021-11-03 15:44:13 +0300

    Add files via upload

---
+++ ocp.py
@@ -0,0 +1,48 @@
+import cv2
+import  pyshine as ps #  pip3 install pyshine==0.0.9
+HTML="""
+<html>
+<head>
+<title>PyShine Live Streaming</title>
+</head>
+
+<body>
+<center><h1> PyShine Live Streaming using OpenCV </h1></center>
+<center><img src="stream.mjpg" width='640' height='480' autoplay playsinline></center>
+</body>
+</html>
+"""
+def main():
+    StreamProps = ps.StreamProps
+    StreamProps.set_Page(StreamProps,HTML)
+    address = ('192.168.1.1',9000) # Enter your IP address 
+    try:
+        StreamProps.set_Mode(StreamProps,'cv2')
+        capture = cv2.VideoCapture(0)
+        capture.set(cv2.CAP_PROP_BUFFERSIZE,4)
+        capture.set(cv2.CAP_PROP_FRAME_WIDTH,320)
+        capture.set(cv2.CAP_PROP_FRAME_HEIGHT,240)
+        capture.set(cv2.CAP_PROP_FPS,30)
+        ret, frame = video_capture.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = faceCascade.detectMultiScale(
+            gray,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30),
+            flags=cv2.CASCADE_SCALE_IMAGE)
+        # Draw a rectangle around the faces
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        StreamProps.set_Capture(StreamProps,capture)
+        StreamProps.set_Quality(StreamProps,90)
+        server = ps.Streamer(address,StreamProps)
+        print('Server started at','http://'+address[0]+':'+str(address[1]))
+        server.serve_forever()
+        
+    except KeyboardInterrupt:
+        capture.release()
+        server.socket.close()
+        
+if __name__=='__main__':
+    main()
